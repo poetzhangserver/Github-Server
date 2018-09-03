c5dd83f40f6834bb57f7df2f721d0b3c99f93fb3
Zhang's Macbook Pro <poetzhangzi@gmail.com>
2018-09-03 21:47:32 +0800

    Oh-info

--- DataManager/test.php
+++ DataManager/test.php
@@ -1,8 +1,5 @@
 <?php
 	require_once(dirname(__FILE__)."/../PathManager/PathManager.php");
-	echo "import success;";
-	echo "file exist:".file_exists("/data");
-	
-	echo "finish;";
+	phpinfo();
 
 ?>
